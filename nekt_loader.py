9d86114758982dd367369700b510dbbc1cc04aa6
Téo Calvo <teocalvo2@gmail.com>
2026-05-18 09:10:03 -0300

    fix: add nekt.engine

--- nekt_loader.py
+++ nekt_loader.py
@@ -9,11 +9,12 @@ dotenv.load_dotenv()
 import nekt
 
 nekt.data_access_token = os.getenv("NEKT_DATA_ACCESS_TOKEN")
-MYSQL_ENGINE = sqlalchemy.create_engine(f'mysql+pymysql://{os.getenv("MYSQL_URI")}')
+nekt.engine = 'spark'
 
-# %%
+MYSQL_ENGINE = sqlalchemy.create_engine(f'mysql+pymysql://{os.getenv("MYSQL_URI")}')
 
 def nekt_to_mysql(layer_name, t_nekt, t_fs, schema, renames):
     fs_table = nekt.load_table(layer_name=layer_name, table_name=t_nekt)
     df= fs_table.toPandas().rename(columns=renames)
     df.to_sql(t_fs, schema=schema, con=MYSQL_ENGINE, if_exists="replace", index=False)
+
